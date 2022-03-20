bb8613c5732cb9887156b92e1c5658549e11b5c6
malaaa <15703476199ma@gmail.com>
2022-03-20 14:58:04 -0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,15 +9,16 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        </p>{" "}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
+          {" "}
           Learn React
-        </a>
+        </a>{" "}
       </header>
     </div>
   );
