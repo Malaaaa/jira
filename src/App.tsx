fde81a9bcfc3efcb9d10c6081c45f1c36aef1861
malaaa <15703476199ma@gmail.com>
2022-03-21 15:12:45 -0400

    feat: login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import ProjectListScreen from "screens/project-list";
+import LoginScreen from "screens/login";
+// import ProjectListScreen from "screens/project-list";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen></ProjectListScreen>
+      {/* <ProjectListScreen /> */}
+      <LoginScreen />
     </div>
   );
 }
