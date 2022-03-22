18ea1ddf18498d03f01687e27769beb58c308e53
malaaa <15703476199ma@gmail.com>
2022-03-22 18:38:33 -0400

    style: Layout  with Row

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,75 +1,41 @@
-import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
+import { useAuth } from "context/auth-context";
 import styled from "@emotion/styled";
-/**
- * The application scenarios of grid and flex
- * 1. to consider, is it a one-dimensional layout or two-dimensional layout
- * Generally speaking, flex is used for one-dimensional layout, grid for two-dimensional layout.
- * 2. from the content or from the layout?
- * From the content: you first have a set of content (the number is generally not fixed), and then you want them to be evenly distributed in the container, the size of the content itself to determine the space occupied
- * from the layout: first plan the grid (the number is generally fixed), and then fill the elements into it
- * From content, use flex
- * From the layout, use grid
- *
- */
+import { Row } from "components/lib";
+
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <Container>
-      <Header>
-        <HeaderLeft>
-          <h3>Logo</h3>
-          <h3>Projects</h3>
-          <h3>User</h3>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <HeaderItem>Logo</HeaderItem>
+          <HeaderItem>Project</HeaderItem>
+          <HeaderItem>User</HeaderItem>
         </HeaderLeft>
         <HeaderRight>
-          <button onClick={logout}>Log out</button>
+          <button onClick={logout}>Log Out</button>
         </HeaderRight>
       </Header>
-      <Nav>nav</Nav>
       <Main>
         <ProjectListScreen />
       </Main>
-      <Aside>aside</Aside>
-      <Footer>footer</Footer>
     </Container>
   );
 };
 
+const HeaderItem = styled.h3`
+  margin-right: 3rem;
+`;
+
 const Container = styled.div`
   display: grid;
-  grid-template-rows: 6rem 1fr 6rem;
-  grid-template-columns: 20rem 1fr 20rem;
-  grid-template-areas:
-    "header header header"
-    "nav main aside"
-    "footer footer footer";
+  grid-template-rows: 6rem 1fr;
   height: 100vh;
-  grid-gap: 10rem;
 `;
 
 // grid-area is used to give names to grid child elements
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-`;
+const Header = styled(Row)``;
+const HeaderLeft = styled(Row)``;
 const HeaderRight = styled.div``;
-const Main = styled.main`
-  grid-area: main;
-`;
-const Nav = styled.nav`
-  grid-area: nav;
-`;
-const Aside = styled.aside`
-  grid-area: aside;
-`;
-const Footer = styled.footer`
-  grid-area: footer;
-`;
+const Main = styled.main``;
--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -26,27 +26,15 @@ export const UnauthenticatedApp = () => {
     </Container>
   );
 };
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  min-height: 100vh;
-`;
-const ShadowCard = styled(Card)`
-  width: 40rem;
-  min-height: 56rem;
-  padding: 3.2rem 4rem;
-  border-radius: 0.3rem;
-  box-shadow: rgba(0, 0, 0, 0.1) 0 0 10px;
-  text-align: center;
-`;
-const Header = styled.header`
-  background: url(${logo}) no-repeat center;
-  padding: 5rem 0;
-  background-size: 8rem;
+export const LongButton = styled(Button)`
   width: 100%;
 `;
+
+const Title = styled.h2`
+  margin-bottom: 2.4rem;
+  color: rgb(94, 108, 132);
+`;
+
 const Background = styled.div`
   position: absolute;
   width: 100%;
@@ -58,7 +46,27 @@ const Background = styled.div`
     calc(((100vw - 40rem) / 2) - 3.2rem), cover;
   background-image: url(${left}), url(${right});
 `;
-const Title = styled.h2`
-  margin-bottom: 2.4rem;
-  color: rgb(94, 108, 132);
+
+const Header = styled.header`
+  background: url(${logo}) no-repeat center;
+  padding: 5rem 0;
+  background-size: 8rem;
+  width: 100%;
+`;
+
+const ShadowCard = styled(Card)`
+  width: 40rem;
+  min-height: 26rem;
+  padding: 3.2rem 4rem;
+  border-radius: 0.3rem;
+  box-sizing: border-box;
+  box-shadow: rgba(0, 0, 0, 0.1) 0 0 10px;
+  text-align: center;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
 `;
