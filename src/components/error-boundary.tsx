ff2b0fddd6ca7f80108982bcf3aef3d4f08125a7
malaaa <15703476199ma@gmail.com>
2022-03-23 21:55:09 -0400

    feat: ADD errorboundary

---
+++ src/components/error-boundary.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+type FallbackRender = (props: { error: Error | null }) => React.ReactElement;
+
+// https://github.com/bvaughn/react-error-boundary
+export class ErrorBoundary extends React.Component<
+  React.PropsWithChildren<{ fallbackRender: FallbackRender }>,
+  { error: Error | null }
+> {
+  state = { error: null };
+
+  // When a child component throws an exception, it is picked up here and called
+  static getDerivedStateFromError(error: Error) {
+    return { error };
+  }
+
+  render() {
+    const { error } = this.state;
+    const { fallbackRender, children } = this.props;
+    if (error) {
+      return fallbackRender({ error });
+    }
+    return children;
+  }
+}
