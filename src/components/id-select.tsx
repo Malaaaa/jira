cb623caf37e1be6adad4a917c837e6df0b22d59f
malaaa <15703476199ma@gmail.com>
2022-03-25 18:35:22 -0400

    feat: fixed id type

---
+++ src/components/id-select.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Raw } from "types";
+import { Select } from "antd";
+
+type SelectProps = React.ComponentProps<typeof Select>;
+
+interface IdSelectProps
+  extends Omit<SelectProps, "value" | "onChange" | "options"> {
+  value: Raw | null | undefined;
+  onChange: (value?: number) => void;
+  defaultOptionName?: string;
+  options?: { name: string; id: number }[];
+}
+
+/**
+ * value String|number|null|undefine
+ * onChange    number|undefined
+ * while isNaN(Number(value)) true use default type
+ * while default type is selected, onChange will call back undefined
+ * @param props
+ * @constructor
+ */
+export const IdSelect = (props: IdSelectProps) => {
+  const { value, onChange, defaultOptionName, options, ...restProps } = props;
+  return (
+    <Select
+      value={toNumber(value)}
+      onChange={(value) => onChange(toNumber(value) || undefined)}
+      {...restProps}
+    >
+      {defaultOptionName ? (
+        <Select.Option value={0}>{defaultOptionName}</Select.Option>
+      ) : null}
+      {options?.map((option) => (
+        <Select.Option key={option.id} value={option.id}>
+          {option.name}
+        </Select.Option>
+      ))}
+    </Select>
+  );
+};
+
+const toNumber = (value: unknown) => (isNaN(Number(value)) ? 0 : Number(value));
